00bc3cd19c3f0017b7f3fc391f3f4037443afb92
keevj <kevin7.world@gmail.com>
2022-12-12 21:13:05 -0400

    Avance pantalla de Inicio

--- src/screeens/HomeScreen.js
+++ src/screeens/HomeScreen.js
@@ -1,12 +1,50 @@
 import React from 'react-native';
-import {View, Text} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import styles from '../styles/screens/HomeScreen.component.style';
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hola Mundo soy el Home de la APK</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* Container iconos */}
+      <View style={styles.containerIconos}>
+        <Text style={styles.text}>Hola, Kevin</Text>
+      </View>
+      {/* Container anuncios */}
+      <View style={styles.containerAnuncio}>
+        <Text style={styles.test}>Hola</Text>
+      </View>
+      {/* Container Eventos */}
+      <ScrollView style={styles.containerEvento}>
+        <View style={styles.containerEventoView}>
+          <Text style={styles.text}>Hola, Evento</Text>
+        </View>
+        <View style={styles.containerEventoView}>
+          <Text style={styles.text}>Hola, Evento</Text>
+        </View>
+        <View style={styles.containerEventoView}>
+          <Text style={styles.text}>Hola, Evento</Text>
+        </View>
+        <View style={styles.containerEventoView}>
+          <Text style={styles.text}>Hola, Evento</Text>
+        </View>
+        <View style={styles.containerEventoView}>
+          <Text style={styles.text}>Hola, Evento</Text>
+        </View>
+        <View style={styles.containerEventoView}>
+          <Text style={styles.text}>Hola, Evento</Text>
+        </View>
+        <View style={styles.containerEventoView}>
+          <Text style={styles.text}>Hola, Evento</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 export default HomeScreen;
--- src/styles/screens/HomeScreen.component.style.js
+++ src/styles/screens/HomeScreen.component.style.js
@@ -7,8 +7,31 @@ export default StyleSheet.create({
     fontSize: theme.font_size_small,
     height: '100%',
   },
+  containerIconos: {
+    backgroundColor: '#235FAD',
+    height: '10%',
+  },
+  containerAnuncio: {
+    backgroundColor: '#FA7364',
+    height: '20%',
+  },
+  containerEvento: {
+    backgroundColor: '#4E4846',
+    height: '60%',
+  },
+  containerEventoView: {
+    backgroundColor: 'yellow',
+    height: 100,
+    marginBottom: 10,
+  },
   text: {
     color: '#FF5733',
     textAlign: 'center',
+    height: '100%',
+  },
+  test: {
+    color: '#4C98FA',
+    textAlign: 'center',
+    height: '100%',
   },
 });
